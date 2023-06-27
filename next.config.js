b0913e0f50ef786b4b9be366c8b3df8ed64c78be
Mac <mac@Macs-MacBook-Pro.local>
2023-06-27 12:45:29 +0500

    changed config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/github-pages",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
